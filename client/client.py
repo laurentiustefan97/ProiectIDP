b242b9b6a8d0a72160c18599180e1862d3079c8a
laurentiustefan97 <steflaurentiu@gmail.com>
2020-03-05 00:02:08 +0200

    partial client implementation

--- client/client.py
+++ client/client.py
@@ -1,32 +1,78 @@
 import paho.mqtt.client as mqtt
+import socket
 
-# The callback for when the client receives a CONNACK response from the server. 
-def on_connect(client, userdata, flags, rc): 
-    print("Connected with result code " + str(rc)) 
- 
-    # Subscribing in on_connect() means that if we lose the connection and 
-    # reconnect then subscriptions will be renewed. 
-    client.subscribe("laur/#") 
- 
-# The callback for when a PUBLISH message is received from the server. 
-def on_message(client, userdata, msg): 
-    print(msg.topic+" "+str(msg.payload)) 
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
 
 def init_mqtt_client():
     client = mqtt.Client()
     client.on_connect = on_connect
-    client.on_message = on_message
     return client
+
+
+def connect_mqtt_broker(client):
+    while True:
+        try:
+            # Connect to the mqtt broker on port 1883
+            client.connect("mqtt-broker", 1883, 60)
+        except socket.gaierror:
+            print("Failed to connect to mqtt broker!")
+            continue
+        break
+
+
+def add_expenditure(client):
+    print('Introduce the product category')
+    category = input()
+
+    print('Introduce the user name')
+    username = input()
+
+    print('Introduce the product name')
+    product_name = input()
+
+    print('Introduce the product price')
+    product_price = input()
+
+    print('Introduce the expenditure date')
+    date = input()
+
+    # TODO send over the MQTT client a json with the data
+    client.publish("idp/" + category, "todo")
+
 
 def main():
     client = init_mqtt_client()
 
-    client.connect('localhost', 1883, 1883)
+    connect_mqtt_broker(client)
 
     client.loop_start()
 
+    print('Press any key to start!')
+    input()
+
+    print('--------------------------------------------------------')
+    print('Welcome to the IDP Expenditure Evidence Service!')
+    print('--------------------------------------------------------')
+
     while True:
-        client.publish("laur/test", input())
+        print()
+        print('----------------------------------------------------')
+        print('Introduce the desired operation:')
+        print('[1]: Add an expenditure')
+        print('[2]: Exit the IDP Expenditure Evidence Service!')
+        print('[x]: More soon!')
+        print('----------------------------------------------------')
+
+        operation = input()
+
+        if operation == '1':
+            add_expenditure(client)
+        elif operation == '2':
+            break
+
 
 if __name__ == '__main__':
     main()
